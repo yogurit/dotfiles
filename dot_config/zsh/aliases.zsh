2b6c5c5ccb391e34c251f5ad6abf2221fab866e3
yogurit <yogurit@outlook.com>
2026-03-19 01:48:29 +0800

    Update .config/tmux/tmux.conf Update .config/zsh/aliases.zsh

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -1,3 +1,6 @@
 # brew bundle
 alias brewdump='brew bundle dump --file=~/.config/brew/Brewfile --force --describe --no-vscode'
 alias brewbundle='brew bundle --file=~/.config/brew/Brewfile'
+
+# tmux
+alias ta='tmux attach'
